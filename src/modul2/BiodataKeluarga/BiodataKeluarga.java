1be2cfefd39f1db82ad116bdf44dd74eb54a3384
User <User@DESKTOP-10CMS>
2018-09-19 14:31:48 +0700

    final

---
+++ src/modul2/BiodataKeluarga/BiodataKeluarga.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul2.BiodataKeluarga;
+
+/**
+ *
+ * @author User
+ */
+public class BiodataKeluarga {
+        private String namaAyah, namaIbu, namaSaudara, namaSaya, Alamat, tempatLahir, hobi, cita_cita;
+    private String tanggalLahir;
+    private String noTelepon;
+
+    public void setNamaAyah(String namaAyah) {
+        this.namaAyah = namaAyah;
+    }
+
+    public void setNamaIbu(String namaIbu) {
+        this.namaIbu = namaIbu;
+    }
+
+    public void setNamaSaudara(String namaSaudara) {
+        this.namaSaudara = namaSaudara;
+    }
+
+    public void setNamaSaya(String namaSaya) {
+        this.namaSaya = namaSaya;
+    }
+
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public void setHobi(String hobi) {
+        this.hobi = hobi;
+    }
+
+    public void setCita_cita(String cita_cita) {
+        this.cita_cita = cita_cita;
+    }
+
+    public void setTanggalLahir(String tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+
+    public String getNamaAyah() {
+        return namaAyah;
+    }
+
+    public String getNamaIbu() {
+        return namaIbu;
+    }
+
+    public String getNamaSaudara() {
+        return namaSaudara;
+    }
+
+    public String getNamaSaya() {
+        return namaSaya;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public String getHobi() {
+        return hobi;
+    }
+
+    public String getCita_cita() {
+        return cita_cita;
+    }
+
+    public String getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+
+    
+}
